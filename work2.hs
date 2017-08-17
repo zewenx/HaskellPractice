3d38b71385a948829b9d04c9de3f7924caff5bb7
Francis <zw.final@gmail.com>
2017-08-17 23:05:20 +1000

    eq deriving instance data constructor show compare

--- work2.hs
+++ work2.hs
@@ -1,7 +1,7 @@
 -- @Author: anchen
 -- @Date:   2017-08-11 15:37:23
 -- @Last Modified by:   anchen
--- @Last Modified time: 2017-08-11 16:12:47
+-- @Last Modified time: 2017-08-17 23:03:50
 
 factorial :: Int -> Int
 factorial n 
@@ -21,3 +21,29 @@ longestPrefix (a:as) (b:bs)
     | otherwise = []
 
 
+data Gender = Male | Female
+
+
+data Suit = Club | Diamond | Heart | Spade deriving Show
+data Rank = R2 | R3 | R4 | R5 | R6 | R7 | R8 | R9 | R10 | Jack | Queen | King | Ace deriving (Eq, Ord) 
+data Card = Card Suit Rank deriving ()
+
+getRank :: Card -> Rank
+getRank (Card a b) = b
+
+xxx = Card Club R5
+printRank :: Rank -> [Char]
+printRank R7 ="7"
+
+printRank2 :: Rank -> String
+printRank2 a 
+ | a == R2 = "2"
+ | otherwise = "7"
+
+oneCard = R2
+twoCard = R5
+
+abool = oneCard > twoCard
+
+instance Show Rank where
+    show = printRank2
