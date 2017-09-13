dbbf803711120318d46759b17761380f23f8ea9c
Zewen Xu <zewenx@student.unimelb.edu.au>
2017-09-13 13:57:25 +1000

    work5

--- work4.hs
+++ work4.hs
@@ -1,28 +1,46 @@
 -- @Author: anchen
 -- @Date:   2017-08-25 12:28:42
 -- @Last Modified by:   anchen
--- @Last Modified time: 2017-08-29 22:48:00
+-- @Last Modified time: 2017-09-13 12:37:35
 
 
-rightTriangles m n = [(a, b, c) | c <- [m..n], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
-mapList = fromListWith (+) [(x,1)|x<-xs] in
+--rightTriangles m n = [(a, b, c) | c <- [m..n], b <- [1..c], a <- [1..b], a^2 + b^2 == c^2]
+--mapList = fromListWith (+) [(x,1)|x<-xs] 
 
--- workshop 4
-transcope :: [[Int]] -> [[Int]]
-transcope (a:as):ss = [(a:transcope ss),transcope as
 
 --lecture week 4
 
 data Tree = Leaf | Node String Int Tree Tree
-data Tree k v = Leaf | Node k v (Tree k v) (Tree k v)
+--data Tree k v = Leaf | Node k v (Tree k v) (Tree k v)
 
-countnodes :: Tree -> Int
+--countnodes :: Tree -> Int
 
-countnodes :: Tree k v -> Int
+--countnodes :: Tree k v -> Int
 
-search_bst :: Tree -> String -> Maybe Int
+--search_bst :: Tree -> String -> Maybe Int
 
 -- it won't work, k v need EQ or Ord
-search_bst :: Tree k v -> k -> Maybe v
+--search_bst :: Tree k v -> k -> Maybe v
 
 -- Struture Induction and Proof
+
+
+-- workshop 4
+--Q1
+
+--Q2 
+transpose :: [[a]] -> [[a]]
+transpose []= []
+transpose s
+    | length (head s) == 0 = []
+    | otherwise = (map head s) : transpose (map tail s)
+
+--Q3
+
+stats ::  [Int] -> (Int,Int,Int)
+stats s = ((length s), (sum s), sum (map (^2) s))
+
+stats2 [] =(0,0,0)
+stats2 (s:ss) = 
+    let (l,p,t) = stats2 ss in
+    (1+l,p+s,t+s*s)
--- work5.hs
+++ work5.hs
@@ -1,7 +1,7 @@
 -- @Author: anchen
 -- @Date:   2017-09-06 09:59:02
 -- @Last Modified by:   anchen
--- @Last Modified time: 2017-09-06 11:33:57
+-- @Last Modified time: 2017-09-13 13:21:58
 
 import Data.List
 -- lecture week 5
@@ -52,10 +52,56 @@ ff3 x
     | (length x) < 3 = []
     | otherwise = [(a,b,c)|a<-x,b<-x,c<-x, a/=b, b /=c , a/= c]
 
---* ????
+--
 --get_names1 :: [Customer] -> [String]
 --get_names1 customers = map customer_name customers
 --
 --get_names2 :: [Customer] -> [String]
 --get_names2 = map customer_name 
 --
+
+--
+--reverse . map (1+) [2,3,5,1,4]
+--reverse . sort [2,3,5,1,4]
+
+--concatr is more efficient than concatl
+
+square x = x ^ 2
+hypotenuse ::Floating a => [a] -> a
+hypotenuse sides = sqrt (sum (map square sides))
+
+hypotenuse' ::Floating a => [a] -> a
+hypotenuse' = sqrt . sum . (map square)
+
+-- workshop5
+--q1
+maybeApply :: (a -> b) -> Maybe a -> Maybe b
+maybeApply f Nothing = Nothing
+maybeApply f (Just a) = Just $ f a
+-- what is the function of $
+
+--q2
+zWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+zWith f [] _ = []
+zWith f _ [] = []
+zWith f  a b =
+    f (head a) (head b) : zWith f (tail a) (tail b)
+
+--Q3
+linearEqn :: Num a => a -> a -> [a] -> [a]
+linearEqn a b = map (\x -> a*x + b ) 
+
+linearEqn1 a b = map (cal a b)
+
+cal :: Num a =>a->a-> a -> a
+cal a b x= a*x +b 
+
+--Q4
+sqrtPM :: (Floating a, Ord a) => a -> [a]
+sqrtPM x
+  | x  > 0    = let y = sqrt x in [y, -y] 
+  | x == 0    = [0]
+  | otherwise = []
+
+
+allSqrts s= foldl (++) [] (map sqrtPM s) 
