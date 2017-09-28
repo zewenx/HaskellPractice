e38d490246deafebfd50c9d43dfecf2f8b7a4227
Zewen Xu <zewenx@student.unimelb.edu.au>
2017-09-28 12:59:41 +1000

    work8

--- work8.pl
+++ work8.pl
@@ -21,7 +21,7 @@ adjacent1(E1, E2, [_E|List]) :-
 %Q5 it is wrong with some cases.
 before(E1, E2, [E1|List]) :- before(_,E2,List).
 before(_,E2,[E2|_]).
-before(E1,E2,[_|List] :- before(E1,E2,List).
+before(E1,E2,[_|Tail]):-before(E1,E2,Tail).
 
 
 %leture 8
@@ -29,3 +29,23 @@ ap(A,B,C) :- (
     A = [] -> C = B
     ; A =[U|V], ap(V,B,VB), C=[U|VB]
     ).
+
+
+calss([],0).
+calss([H|T],X) :- 
+    int(H),
+    X>0,
+    Tail is X-H,
+    calss(T,Tail).
+
+
+int(0).
+int(1).
+int(2).
+int(3).
+int(4).
+int(5).
+int(6).
+int(7).
+int(8).
+int(9).
